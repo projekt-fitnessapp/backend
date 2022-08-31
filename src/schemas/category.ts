0b07b57ca645e2f5fbf730016130b0931c4511b8
lumaghg <rotkehlchen567@gmail.com>
2022-08-31 15:52:06 +0200

    demo

--- src/schemas/category.ts
+++ src/schemas/category.ts
@@ -3,8 +3,9 @@ import { CategoryDocument, CategoryModel } from '../types/db/category.types';
 
 const CategorySchema = new Schema({
     name: { type: String, required: true },
-    sku: { type: Number, 
-        default: null }
+    sku: { type: Number, default: null }
 });
+
+console.log("Hello")
 
 export const Category = mongoose.model<CategoryDocument, CategoryModel>('Category', CategorySchema, 'categories');
