87e3c2a39f7e1a2a4bb0aeeb8e4f17a56fce193e
Hase Florian <florian.hase@sap.com>
2022-09-30 22:26:14 +0200

    added lastTrainingRouter to AppRouter

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { PingRouter } from "./ping/ping.router";
+import { PingRouter } from './ping/ping.router';
+import { LastTrainingRouter } from './lastTraining/lastTraining.router';
 
 const router = Router();
 
-router.use("/ping", PingRouter);
+router.use('/ping', PingRouter);
+router.use('/lastTraining', LastTrainingRouter);
 
 export { router as AppRouter };
