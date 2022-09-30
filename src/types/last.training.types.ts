a58b05b53bff989b73c8a1c5ff1b894f3d87ab87
Hase Florian <florian.hase@sap.com>
2022-09-30 23:33:37 +0200

    removed unnecessary code

--- src/types/last.training.types.ts
+++ src/types/last.training.types.ts
@@ -1,10 +1,4 @@
-import { Document, Model } from 'mongoose';
-
 export type TLastTraining = {
     date: string,
     trained: boolean
 }
-
-export type LastTrainingDocument = Document<TLastTraining>;
-
-export type LastTrainingModel = Model<LastTrainingDocument>;
