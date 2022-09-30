836138699f00b4910c4f0e183a48a4a15b4667c4
Hase Florian <florian.hase@sap.com>
2022-09-30 19:05:11 +0200

    removed category boilerplate

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
 
-import { CategoryRouter } from "./category/category.router";
 import { PingRouter } from "./ping/ping.router";
 
 const router = Router();
 
-router.use("/categories", CategoryRouter);
 router.use("/ping", PingRouter);
 
 export { router as AppRouter };
