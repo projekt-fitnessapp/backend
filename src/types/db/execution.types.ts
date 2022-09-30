7a135954bc73de71d13455841df0da9387518734
Jannis Jüngert <73344489+juengeja@users.noreply.github.com>
2022-09-30 11:20:38 +0200

    type correction

--- src/types/db/execution.types.ts
+++ src/types/db/execution.types.ts
@@ -3,14 +3,14 @@ import { TExcercise } from './exercise.types';
 
 export type TSet = {
     executionType: {type: string, enum: ['warmup', 'working', 'backoff']},
-    weight: {type: Number, min: 0},
-    reps: {type: Number, min: 0},
-    tenRM: {type: Number, min: 0}
+    weight: {type: number, min: 0},
+    reps: {type: number, min: 0},
+    tenRM: {type: number, min: 0}
 }
 
 export type TExecution = {
     exercise: TExcercise,
-    notes: [String],
+    notes: [string],
     sets: [TSet]
 }
 
