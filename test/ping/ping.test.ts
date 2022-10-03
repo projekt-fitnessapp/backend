216fa1d1526fa7b62728d9641c7e552133ae43d0
Hase Florian <florian.hase@sap.com>
2022-10-03 19:59:07 +0200

    proposal to fix test bug

--- test/ping/ping.test.ts
+++ test/ping/ping.test.ts
@@ -1,7 +1,9 @@
+process.env.PORT = "3000";
 import { describe, test } from "mocha";
 import { server } from "../../src/app";
 import supertest from "supertest";
 import { expect } from "chai";
+
 const requestWithSupertest = supertest(server);
 
 describe("Ping Endpoint Test", () => {
