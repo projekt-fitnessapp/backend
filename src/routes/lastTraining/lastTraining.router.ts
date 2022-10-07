7ca64364491c61e260eb6e456760214f1ae46e30
Hase Florian <florian.hase@sap.com>
2022-10-07 19:13:36 +0200

    removed test-route

--- src/routes/lastTraining/lastTraining.router.ts
+++ src/routes/lastTraining/lastTraining.router.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
-import { subDays } from '../../helpers/dates';
 import { getLastTraining } from './lastTraining.controller';
 
 const router = Router();
 
 router.get('/', getLastTraining);
-router.get('/a/', () => subDays(Date.now(), 10));
 
 export { router as LastTrainingRouter };
