1b3abc1372bb72dd52788ea97ce19b58b09da058
Vollomat <patrick.vollstedt@gmx.de>
2022-10-09 19:45:48 +0200

    Get-Methode implementiert

--- src/routes/account/account.controller.ts
+++ src/routes/account/account.controller.ts
@@ -1,47 +1,45 @@
 import { Request, Response } from 'express';
-import { Account } from '../../types/last.training.types';
 
-
+import { TAccount } from '../../types/db/account.types';
 import { Account } from '../../schemas/account';
 
 
 export async function getAccount(
-    req: { query: { days: string } },
+    req: { query: { userId: string } },
     res: {
       status: (statusCode: number) => void;
-      json: (responseBody: { data: TLastTraining[] }) => any;
+      json: (responseBody: { data: TAccount }) => any;
       statusMessage: string;
     }
   ) {
-    if (req.query.days && !isNaN(parseInt(req.query.days as string))) {
-      const days = parseInt(req.query.days as string);
-      const now = Date.now();
-      const startDate = formatISO(subDays(now, days));
+    //Die UserID darf nicht leer sein!
+    if (req.query.userId) {
+      const userId = req.query.userId;
       const docs = JSON.parse(
         JSON.stringify(
-          await TrainingSession.find({
-            date: { $gte: startDate },
+          await Account.find({
+            _id: { $eq: userId },
           })
             .lean()
-            .select('date')
+            .select('_id')
             .exec()
         )
       );
   
-      let resBody = Array<TLastTraining>();
-  
-      for (let index = 0; index < days; index++) {
-        const dateToCheck = formatISO(subDays(now, index)).split('T')[0];
-        let trained = false;
-        docs.forEach((trainingSession: { _id: string; date: string }) => {
-          const entryDate = trainingSession.date;
-          if (entryDate.includes(dateToCheck)) {
-            trained = true;
-          }
-        });
-        resBody.push({ date: dateToCheck, trained: trained });
+      let resBody = {} as TAccount;
+      if(docs) {
+        resBody._id= userId;
+        resBody.google_id = docs.gooogle_id;
+        resBody.name = docs.name;
+        resBody.birthdate = docs.birthdate; 
+        resBody.sex = docs.sex;
+        resBody.trainingPlans = docs.trainingPlans;
+      } else {
+      res.status(400);
+        res.statusMessage = 'no account found!';
+        return;
       }
-  
+      
       res.status(200);
       res.json({ data: resBody });
       return;
