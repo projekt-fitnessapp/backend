dac6c64ed126354abe53e7c710d4fa5efdf24afc
Jannis Jüngert <73344489+juengeja@users.noreply.github.com>
2022-10-16 15:51:35 +0200

    myPlans route

---
+++ src/routes/myPlans/myPlans.controller.ts
@@ -0,0 +1,36 @@
+import { Account } from "../../schemas/account";
+import { Request, Response } from 'express';
+
+export async function getMyPlans(
+    req: Request,
+    res: Response
+    ){
+        try {
+            if(!req.query.userId){
+                res.statusMessage = "No User Id provided"
+                return res.sendStatus(400)
+            }
+            const account = await Account.findById(req.query.userId)
+            .populate({
+                path: 'trainingPlans',
+                populate: {
+                    path: 'trainingDays',
+                    model: 'TrainingDay',
+                    populate: {
+                        path: 'exercises',
+                        model: 'Exercise'
+                    }
+                }
+            })
+
+            if(!account){
+                res.statusMessage = "No User with that Id"
+                return res.sendStatus(404)
+            }
+            res.statusMessage = "Users TrainingPlans list"
+            return res.json(account._id.trainingPlans)
+            
+        } catch (error) {
+            return res.sendStatus(400)
+        }
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { PingRouter } from "./ping/ping.router";
 import { TrainingSessionRouter } from "./trainingSession/trainingSession.router";
+import { MyPlansRouter } from "./myPlans/myPlans.router";
 
 const router = Router();
 
 router.use("/ping", PingRouter);
 router.use('/trainingSession', TrainingSessionRouter)
+router.use('/myPlans', MyPlansRouter)
 
 export { router as AppRouter };
