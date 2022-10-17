d205749f602a5c61fb3f16fa955c4c7d9d6258a6
Hase Florian <florian.hase@sap.com>
2022-10-17 23:47:16 +0200

    reduced min value for next day

--- src/schemas/training.plan.ts
+++ src/schemas/training.plan.ts
@@ -5,7 +5,7 @@ const TrainingPlanSchema = new Schema({
     name: { type: String, required: true },
     split: { type: Number, min: 1, max: 10, required: true },
     trainingDays: { type: [String], minItems: 1, maxItems: 10, required: true },
-    nextDay: { type: Number, min: 1, max: 10, required: true }
+    nextDay: { type: Number, min: 0, max: 10, required: true }
 });
 
 export const TrainingPlan = mongoose.model<TrainingPlanDocument, TrainingPlanModel>('Training Plan', TrainingPlanSchema, 'training plans');
