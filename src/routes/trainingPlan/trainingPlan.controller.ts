eb9874c27f5dd64f9f87f39ad8702e9ac00f0a03
Hase Florian <florian.hase@sap.com>
2022-10-18 10:06:37 +0200

    fixed populate

--- src/routes/trainingPlan/trainingPlan.controller.ts
+++ src/routes/trainingPlan/trainingPlan.controller.ts
@@ -2,65 +2,72 @@ import { Request, Response } from 'express';
 import { Account } from '../../schemas/account';
 import { TrainingPlan } from '../../schemas/training.plan';
 
-export async function getTrainingPlan(
-  req: Request,
-  res: Response
-) {
-  try{
-    if(!req.query.trainingPlanId) throw new Error("No trainingPlanId provided!");
-    res.status(200).json(await TrainingPlan.findById(req.query.trainingPlanId).populate({
-      path: 'trainingDays',
-      model: 'Training Day'
-    }))
+export async function getTrainingPlan(req: Request, res: Response) {
+  try {
+    if (!req.query.trainingPlanId) {
+      console.log(req.query.trainingPlanId);
+      throw new Error('No trainingPlanId provided!');
+    }
+    console.log('there');
+    return res
+      .status(200)
+      .json(
+        await TrainingPlan.findById(req.query.trainingPlanId).populate(
+          'trainingDays'
+        )
+      );
   } catch (e) {
-    res.statusMessage = "No trainingPlanId provided"
-    res.status(400).send()
+    console.log('here');
+    res.statusMessage = 'No trainingPlanId provided';
+    return res.status(400).send(e);
   }
-  
 }
 
-export async function postTrainingPlan(
-  req: Request,
-  res: Response
-) {
+export async function postTrainingPlan(req: Request, res: Response) {
   try {
-    let id
-    if(req.query.userId){
-      const account = await Account.findById(req.query.userId)
-      id = (await TrainingPlan.create(req.body))._id.toString()
+    let id;
+    if (req.query.userId) {
+      const account = await Account.findById(req.query.userId);
+      id = (await TrainingPlan.create(req.body))._id.toString();
       if (!id) {
-        throw new Error("Malformed TrainingPlan");
+        throw new Error('Malformed TrainingPlan');
       }
       if (account != null) {
-        const filter = { _id: req.query.userId }
-        const resBody = await Account.findOneAndUpdate(filter, { $push: { trainingPlans: id }}, { new: true })
+        const filter = { _id: req.query.userId };
+        const resBody = await Account.findOneAndUpdate(
+          filter,
+          { $push: { trainingPlans: id } },
+          { new: true }
+        );
         if (!resBody) {
-          await TrainingPlan.findByIdAndDelete(id)
-          throw new Error("Update went wrong")
+          await TrainingPlan.findByIdAndDelete(id);
+          throw new Error('Update went wrong');
         }
       } else {
-        await TrainingPlan.findByIdAndDelete(id)
-        throw new Error("No Account with that Id!")
+        await TrainingPlan.findByIdAndDelete(id);
+        throw new Error('No Account with that Id!');
       }
     } else {
-      id = (await TrainingPlan.create(req.body))._id._id
+      id = (await TrainingPlan.create(req.body))._id._id;
     }
-    res.status(201).send(id)
+    res.status(201).send(id);
   } catch (e) {
-    res.status(400).send()
+    res.status(400).send();
   }
 }
 
-export async function putTrainingPlan(
-  req: Request,
-  res: Response
-) {
+export async function putTrainingPlan(req: Request, res: Response) {
   try {
-    if(!req.query.trainingPlanId) throw new Error("No trainingPlanId provided!");
-    const filter = {_id: req.query.trainingPlanId}
-    res.status(201).send(await TrainingPlan.findOneAndUpdate(filter, req.body, {new: true}))
+    if (!req.query.trainingPlanId)
+      throw new Error('No trainingPlanId provided!');
+    const filter = { _id: req.query.trainingPlanId };
+    res
+      .status(201)
+      .send(
+        await TrainingPlan.findOneAndUpdate(filter, req.body, { new: true })
+      );
   } catch (error) {
-    res.statusMessage = "No trainingPlanId provided"
-    res.status(400).send()
+    res.statusMessage = 'No trainingPlanId provided';
+    res.status(400).send();
   }
 }
