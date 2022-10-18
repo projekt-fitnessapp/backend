7e56b193cc13108ded09abf32baf826f6bec169d
Hase Florian <florian.hase@sap.com>
2022-10-18 16:39:30 +0200

    fixed type errors

--- src/schemas/training.plan.ts
+++ src/schemas/training.plan.ts
@@ -1,11 +1,25 @@
 import mongoose, { Schema } from 'mongoose';
-import { TrainingPlanDocument, TrainingPlanModel } from '../types/db/training.plan.types';
+import {
+  TrainingPlanDocument,
+  TrainingPlanModel,
+} from '../types/db/training.plan.types';
 
 const TrainingPlanSchema = new Schema({
-    name: { type: String, required: true },
-    split: { type: Number, min: 1, max: 10, required: true },
-    trainingDays: { type: Schema.Types.ObjectId[], ref: 'Training Day', minItems: 1, maxItems: 10, required: true },
-    nextDay: { type: Number, min: 0, max: 10, required: true }
+  name: { type: String, required: true },
+  split: { type: Number, min: 1, max: 10, required: true },
+  trainingDays: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Training Day',
+      minItems: 1,
+      maxItems: 10,
+      required: true,
+    },
+  ],
+  nextDay: { type: Number, min: 0, max: 10, required: true },
 });
 
-export const TrainingPlan = mongoose.model<TrainingPlanDocument, TrainingPlanModel>('Training Plan', TrainingPlanSchema, 'training plans');
+export const TrainingPlan = mongoose.model<
+  TrainingPlanDocument,
+  TrainingPlanModel
+>('Training Plan', TrainingPlanSchema, 'training plans');
