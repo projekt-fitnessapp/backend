a62028b3a60317e58a031219cf15eea89f4fa8db
Hase Florian <florian.hase@sap.com>
2022-10-18 16:47:31 +0200

    rerun pipe

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app = setupServer(false);
 
 connectDB(false);
 
+
 app.listen(parseInt(process.env.PORT as string), () => {
   console.log(`App started on  http://localhost:${process.env.PORT}`);
 });
-
