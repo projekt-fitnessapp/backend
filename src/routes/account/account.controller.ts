292c0bd6611e09b3e6f366e36f8afe39fcef801d
Hase Florian <florian.hase@sap.com>
2022-10-19 14:31:13 +0200

    run boy run

--- src/routes/account/account.controller.ts
+++ src/routes/account/account.controller.ts
@@ -24,7 +24,7 @@ export async function saveAccount(req: Request, res: Response) {
 
       return res.status(201).json({ userId });
     } else {
-      throw new Error();
+      throw new Error("Save failed");
     }
   } catch (e) {
     return res.status(400).json(e);
