0c683327fc4c5224d93cb5d702c8243ae0bbbe9a
Hase Florian <florian.hase@sap.com>
2023-02-28 13:56:39 +0100

    fix rebekka

--- src/routes/body/body.controller.ts
+++ src/routes/body/body.controller.ts
@@ -1,15 +1,14 @@
 import { Request, Response } from 'express';
 import { Body } from '../../schemas/body';
 
-export async function getBody(
-  req: Request,
-  res: Response
-) {
-  try{
-    if(!req.query.userId) {
-      throw new Error;
+export async function getBody(req: Request, res: Response) {
+  try {
+    if (!req.query.userId) {
+      throw new Error();
     }
-    const docs =  await Body.find({ userId: req.query.userId }).sort({date: -1});
+    const docs = await Body.find({ userId: req.query.userId }).sort({
+      date: -1,
+    });
     res.status(200);
     return res.json(docs[0]);
   } catch (e) {
@@ -18,13 +17,12 @@ export async function getBody(
   }
 }
 
-export async function saveBody(
-  req: Request,
-  res: Response
-) {
-
+export async function saveBody(req: Request, res: Response) {
   try {
+    const reqBody = req.body;
+    if (reqBody._id == '') delete reqBody._id;
     const savedBody = await Body.create(req.body);
+
     const bodyId = savedBody._id;
     res.status(201);
     return res.json({ bodyId });
