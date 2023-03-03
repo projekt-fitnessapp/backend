a05ce90409993d8d6a26936bafaaadb1c3a1623d
lumaghg <rotkehlchen567@gmail.com>
2023-03-03 09:54:37 +0100

    add winston transport for logging to mongodb

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import logger from './helpers/logging';
 import loggingMiddleware from './middleware/logging.middleware';
 //@ts-expect-error no type declarations available
 import winstonVisualize from 'winston-visualize'
+//import { Request, Response } from 'express';
 
 
 const app = setupServer(false);
@@ -11,19 +12,22 @@ const app = setupServer(false);
 /**
  * Begin Important Logging Code do not remove or alter 
  */
+
+
 function getUnauthorizedResponse() {
   return 'You shall not pass'
 }
-
 app.use('/logs*', basicAuth({
   users: { 'logs': process.env.LOGS_PASSWORD as string },
   challenge: true,
   unauthorizedResponse: getUnauthorizedResponse
 }))
 
+
 app.use(loggingMiddleware);
 
 winstonVisualize(app, logger);
+//app.use('/logs', serveAdminJsLoggingDashboard)
 
 process.on("uncaughtExceptionMonitor", (err)=>{
   logger.log('error', err.toString());
@@ -40,3 +44,10 @@ connectDB(false, logger);
 app.listen(parseInt(process.env.PORT as string), () => {
   logger.log('info', `App started on  http://localhost:${process.env.PORT}`);
 });
+
+/*
+function serveAdminJsLoggingDashboard(req: Request, res: Response) {
+
+
+}
+*/
--- src/helpers/logging.ts
+++ src/helpers/logging.ts
@@ -1,4 +1,36 @@
 import {createLogger, format, transports} from 'winston'
+import dotenv from 'dotenv'
+import Transport from "winston-transport"
+import {MongoClient} from "mongodb"
+require('winston-mongodb')
+
+dotenv.config()
+
+class MongoTransport extends Transport {
+    private dbclient: MongoClient; 
+    constructor(opts: any, dbUrl: string) {
+        //connect to logging db
+        super(opts);
+        this.dbclient = new MongoClient(dbUrl)
+        this.dbclient.connect()
+      }
+    //@ts-expect-error no typings
+      async log(info, callback) {
+        setImmediate(() => {
+          this.emit('logged', info);
+        });
+
+        //convert timestamp to Date and convert to MEZ
+        if(info.timestamp) {
+            info.timestamp = new Date(new Date(info.timestamp).getTime() + 60 * 60 * 1000)
+        }
+        await this.dbclient.db('log').collection('log').insertOne(info)
+    
+        callback();
+      }
+}
+
+
 
 const logger = createLogger({
     format: format.combine(
@@ -7,10 +39,15 @@ const logger = createLogger({
     ),
     transports: [
         new (transports.File)({
-            filename: 'winston.log'
+            filename: 'winston.log',
         }),
         new (transports.Console)()
     ]
   });
+
+//Only log to mongo in prod
+if(process.env.LOGS_MONGO_ACTIVE == 'true') {
+    logger.add(new MongoTransport({}, process.env.LOGS_DB_URL as string))
+}
 
 export default logger
