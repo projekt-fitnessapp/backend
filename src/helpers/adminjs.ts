568050bccd021754858bc328f6df3faae4b3561b
lumaghg <rotkehlchen567@gmail.com>
2023-03-03 11:55:41 +0100

    fix not logging /admin reqs

--- src/helpers/adminjs.ts
+++ src/helpers/adminjs.ts
@@ -1,7 +1,37 @@
 import AdminJSExpress from '@adminjs/express'
 import * as AdminJSMongoose from '@adminjs/mongoose'
 import AdminJS from 'adminjs'
-import { LogEntry } from './logging.entity'
+import { model, Schema } from 'mongoose'
+import { LogLevels } from './logging'
+
+
+interface ILogEntry {
+    message: string
+    timestamp: Date
+    level: LogLevels
+    path?: string
+    statusCode?: number
+    time?: number
+    method?: string
+    payload?: any
+}
+
+
+const LogEntrySchema = new Schema<ILogEntry>(
+    {
+        message: { type: 'String', required: true },
+        timestamp: { type: 'Date', required: true },
+        level: { type: 'String', required: false, enum: LogLevels },
+        path: { type: 'String', required: false },
+        statusCode: { type: 'Number', required: false },
+        time: { type: 'Number', required: false },
+        method: { type: 'String', required: false },
+        payload: { type: 'Object', required: false }
+    },
+    { timestamps: true },
+)
+
+const LogEntry = model<ILogEntry>('LogEntry', LogEntrySchema, 'logs')
 
 AdminJS.registerAdapter({
     Resource: AdminJSMongoose.Resource,
