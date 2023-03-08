1f0479ed8596f30df43eb1501eb4726398e6242b
lumaghg <rotkehlchen567@gmail.com>
2023-03-08 16:21:52 +0100

    exclude /ping and /coverage requests

--- src/middleware/logging.middleware.ts
+++ src/middleware/logging.middleware.ts
@@ -3,7 +3,7 @@ import responseTime from 'response-time'
 import express from 'express';
 
 export default responseTime((req: express.Request, res: express.Response, time: number) => {
-    if (!req.originalUrl.startsWith("/admin")) {
+    if (!(req.originalUrl.startsWith("/admin") || req.originalUrl.startsWith("/ping") || req.originalUrl.startsWith("/coverage"))) {
         if (res.statusCode > 350) {
             logger.log('error', `${req.method} ${req.originalUrl}`, {
                 path: req.originalUrl,
