90e16add637ede46a5de9e5a8f0e796c44937d3b
Hase Florian <florian.hase@sap.com>
2023-03-15 13:14:02 +0100

    added route

--- src/routes/trainingPlan/trainingPlan.router.ts
+++ src/routes/trainingPlan/trainingPlan.router.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import { getTrainingPlan } from './trainingPlan.controller';
 import { postTrainingPlan } from './trainingPlan.controller';
 import { putTrainingPlan } from './trainingPlan.controller';
+import { deleteTrainingPlan } from './trainingPlan.controller';
 
 const router = Router();
 
 router.get('/', getTrainingPlan);
 router.post('/', postTrainingPlan);
 router.put('/', putTrainingPlan);
+router.delete('/', deleteTrainingPlan)
 
 export { router as TrainingPlanRouter };
